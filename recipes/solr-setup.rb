193acf543843027a127cb859a8b9e855e242173f
Shane Davis <shane.davis@linkdigital.com.au>
2016-06-28 18:03:46 +1000

    Tweaking Node DNS config.

--- recipes/solr-setup.rb
+++ recipes/solr-setup.rb
@@ -39,7 +39,7 @@ bash "Add #{service_name} DNS entry" do
 		zoneid=$(aws route53 list-hosted-zones-by-name --dns-name "#{node['datashades']['tld']}" | jq '.HostedZones[0].Id' | tr -d '"/hostedzone')
 		hostcount=$(($(aws route53 list-resource-record-sets --hosted-zone-id $zoneid --query "ResourceRecordSets[?contains(Name, '#{node['datashades']['version']}#{service_name}')].Name" | jq '. | length') + 1))
 		echo "#{service_name}_name=#{node['datashades']['version']}#{service_name}${hostcount}.#{node['datashades']['tld']}" >> /etc/hostnames
-		echo ${hostcount} > /etc/solrid
+		echo ${hostcount} > /etc/#{service_name}id
 	EOS
 	not_if "grep -q '#{service_name}_name' /etc/hostnames"
 end
