31749b9f29928ef03f714ec2534c9b61f28bbac3
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-14 13:16:56 +1000

    [QOL-4495] move EFS mount point to application-specific location

--- recipes/efs-setup.rb
+++ recipes/efs-setup.rb
@@ -31,7 +31,7 @@ directory '/data' do
 end
 
 mount '/data' do
-	device "#{node['datashades']['version']}efs.#{node['datashades']['tld']}:/"
+	device "#{node['datashades']['version']}efs.#{node['datashades']['tld']}:/#{@app_name}/"
 	fstype "nfs4"
 	options "nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
 	action [:mount, :enable]
