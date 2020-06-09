4239318951a93a9fe38d1e3011af3b0803893184
Nathan Perry <nathan@salsadigital.com.au>
2020-06-09 17:32:45 +1200

    [DQL2-13] Removed incorrect comment

--- recipes/ckanweb-deploy-exts.rb
+++ recipes/ckanweb-deploy-exts.rb
@@ -243,16 +243,16 @@ search("aws_opsworks_app", 'shortname:*ckanext*').each do |app|
 		bash "YTP CKAN ext database init" do
 			user "#{account_name}"
 			code <<-EOS
-				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments initdb -c #{config_dir}/production.ini || echo 'Ignoring expected error, see https://github.com/frictionlessdata/ckanext-validation/issues/44'
-				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments init_notifications_db -c #{config_dir}/production.ini || echo 'Ignoring expected error, see https://github.com/frictionlessdata/ckanext-validation/issues/44'
-				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments updatedb -c #{config_dir}/production.ini || echo 'Ignoring expected error, see https://github.com/frictionlessdata/ckanext-validation/issues/44'
+				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments initdb -c #{config_dir}/production.ini'
+				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments init_notifications_db -c #{config_dir}/production.ini'
+				#{virtualenv_dir}/bin/paster --plugin=ckanext-ytp-comments updatedb -c #{config_dir}/production.ini'
 			EOS
 			only_if { "#{pluginname}".eql? 'ytp-comments' }
 		end
 
 		execute "Harvest CKAN ext database init" do
 			user "#{account_name}"
-			command "#{virtualenv_dir}/bin/paster --plugin=ckanext-harvest harvester initdb -c #{config_dir}/production.ini || echo 'Ignoring expected error, see https://github.com/frictionlessdata/ckanext-validation/issues/44'"
+			command "#{virtualenv_dir}/bin/paster --plugin=ckanext-harvest harvester initdb -c #{config_dir}/production.ini"
 			only_if { "#{pluginname}".eql? 'harvest' }
 		end
 
