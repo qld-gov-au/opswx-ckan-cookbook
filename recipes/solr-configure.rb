0fa26c823965da102b0d7135da8103b417ed6a35
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-07-27 16:45:25 +1000

    [QOL-4841] add script to detect the current Solr master based on health checks
    - Use the healthy Solr server with the alphabetically first hostname.
    NB Load-scaled instances have a different naming convention and are not included.

--- recipes/solr-configure.rb
+++ recipes/solr-configure.rb
@@ -31,6 +31,13 @@ template "/usr/local/bin/solr-healthcheck.sh" do
 	mode "0755"
 end
 
+template "/usr/local/bin/pick-solr-master.sh" do
+	source "pick-solr-master.sh.erb"
+	owner "root"
+	group "root"
+	mode "0755"
+end
+
 file "/etc/cron.daily/archive-solr-logs-to-s3" do
 	content "/usr/local/bin/archive-logs.sh #{service_name} >/dev/null 2>&1\n"
 	owner "root"
