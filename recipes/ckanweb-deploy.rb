d13f2468c9bdf373d892a1c5dd847478ec820147
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-03-17 13:51:21 +1000

    [QOL-7841] oops fix name of function whitelist file

--- recipes/ckanweb-deploy.rb
+++ recipes/ckanweb-deploy.rb
@@ -186,8 +186,8 @@ link "#{config_dir}/who.ini" do
 	link_type :symbolic
 end
 
-cookbook_file "#{config_dir}/allowed_files.txt" do
-	source 'allowed_files.txt'
+cookbook_file "#{config_dir}/allowed_functions.txt" do
+	source 'allowed_functions.txt'
 	owner "#{service_name}"
 	group "#{service_name}"
 	mode "0755"
