9703efe5b473d337ff66eca15e18b936687db364
william dutton <will.dutt@gmail.com>
2021-10-18 11:16:13 +1000

    [DQL3-52] reduce from daily to weekly monday per requirements

--- recipes/ckanbatch-deploy.rb
+++ recipes/ckanbatch-deploy.rb
@@ -124,8 +124,8 @@ end
 # Run dataset require updates notifications at 7am and 7:15am on batch
 if File.foreach(config_file).grep(/^\s*ckan[.]plugins\s*=.*\bdata_qld(_integration)?\b/).any?
     file "/etc/cron.d/ckan-dataset-notification-due" do
-        content "00 7 * * * root /usr/local/bin/pick-job-server.sh && PASTER_PLUGIN=ckanext-data-qld #{ckan_cli} send_email_dataset_due_to_publishing_notification >/dev/null 2>&1\n"\
-                "15 7 * * * root /usr/local/bin/pick-job-server.sh && PASTER_PLUGIN=ckanext-data-qld #{ckan_cli} send_email_dataset_overdue_notification >/dev/null 2>&1\n"
+        content "00 7 * * MON root /usr/local/bin/pick-job-server.sh && PASTER_PLUGIN=ckanext-data-qld #{ckan_cli} send_email_dataset_due_to_publishing_notification >/dev/null 2>&1\n"\
+                "15 7 * * MON root /usr/local/bin/pick-job-server.sh && PASTER_PLUGIN=ckanext-data-qld #{ckan_cli} send_email_dataset_overdue_notification >/dev/null 2>&1\n"
         mode '0644'
         owner "root"
         group "root"
