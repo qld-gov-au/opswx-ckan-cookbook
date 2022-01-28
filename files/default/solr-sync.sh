b198688b8ab940f6cb5c27d0650e6bcfa20bbc35
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-01-28 12:05:01 +1000

    [CHG-11123][QOL-8406] wait more robustly for replication to finish
    - check replication status each second instead of just waiting
    - allow up to 20 seconds, error out if not successful
    - improve comments

--- files/default/solr-sync.sh
+++ files/default/solr-sync.sh
@@ -17,6 +17,19 @@ function set_dns_primary () {
   updatedns &
 }
 
+function wait_for_replication_success () {
+  # wait up to 20 seconds for backup to complete, should only take a second or two
+  BACKUP_STATUS=unknown
+  for i in {1..20}; do
+    if [ "$BACKUP_STATUS" = "unknown" ]; then
+      DETAILS=$(curl "$HOST/$CORE_NAME/replication?command=details")
+      echo "$DETAILS" |grep 'status[^a-zA-Z]*success' && return 0
+      echo "$DETAILS" |grep 'exception.*snapshot' && return 1
+      sleep 1
+    fi
+  done
+}
+
 # we can't perform any replication operations if Solr is stopped
 if ! (curl -I --connect-timeout 5 "$HOST/$CORE_NAME/admin/ping" 2>/dev/null |grep '200 OK' > /dev/null); then
   set_dns_primary false
@@ -25,14 +38,19 @@ fi
 sudo mkdir -p "$LOCAL_DIR"
 sudo chown solr "$LOCAL_DIR"
 if (/usr/local/bin/pick-solr-master.sh); then
+  # point traffic to this instance
   set_dns_primary true
-  curl "$HOST/$CORE_NAME/replication?command=backup&location=$LOCAL_DIR&name=$BACKUP_NAME"
-  sleep 5
+
+  # export a snapshot of the index and verify its integrity,
+  # then copy to EFS so secondary servers can read it
+  curl "$HOST/$CORE_NAME/replication?command=backup&location=$LOCAL_DIR&name=$BACKUP_NAME" | grep 'status[^a-zA-Z]*OK' || exit 1
+  wait_for_replication_success || exit 1
   sudo -u solr sh -c "$LUCENE_CHECK $LOCAL_SNAPSHOT && rsync -a --delete '$LOCAL_SNAPSHOT' '$SYNC_SNAPSHOT'" || exit 1
+
+  # clean up - remove old snapshots, hourly backup to S3
   for old_snapshot in $(ls -d $SYNC_DIR/snapshot.$CORE_NAME-* |grep -v "$SNAPSHOT_NAME"); do
     sudo -u solr rm -r "$old_snapshot"
   done
-  # sweep backups to S3 every hour
   if [ "$MINUTE" = "00" ]; then
     cd "$LOCAL_DIR"
     tar --force-local -czf "$SNAPSHOT_NAME.tgz" "$SNAPSHOT_NAME"
@@ -40,6 +58,7 @@ if (/usr/local/bin/pick-solr-master.sh); then
     sudo -u solr rm -r snapshot.$CORE_NAME-*
   fi
 else
+  # make traffic come to this instance only as a backup option
   set_dns_primary false
   # Give the master time to update the sync copy; run halfway between exports
   sleep 30
