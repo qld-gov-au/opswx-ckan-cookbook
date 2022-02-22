c3b8a9ccca9dbb78cc54f14acdc913a5badfa60f
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-22 14:14:23 +1000

    [QOL-8638] ensure unique Chef resource name when moving directory

--- resources/move_and_link.rb
+++ resources/move_and_link.rb
@@ -30,7 +30,8 @@ property :client_service, [String, nil], default: nil
 action :create do
 	if not ::File.identical?(new_resource.source, new_resource.target) then
 		if new_resource.client_service then
-			service new_resource.client_service do
+			service "Stop #{new_resource.client_service} before moving #{new_resource.source}" do
+				service_name new_resource.client_service
 				action [:stop]
 			end
 		end
