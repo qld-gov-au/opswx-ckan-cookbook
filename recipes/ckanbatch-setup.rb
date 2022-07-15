ac956c0f2facfc67e57ded2d21e89cfe6a569a2c
antuarc <carl.antuar@smartservice.qld.gov.au>
2022-07-15 17:00:53 +1000

    [QOL-9055] improve detection of systemd
    - We need a test that will work equally well before the cookbook executes, so 'which systemctl' is better than checking for the initd config file

--- recipes/ckanbatch-setup.rb
+++ recipes/ckanbatch-setup.rb
@@ -41,16 +41,7 @@ bash "Enable Supervisor file inclusions" do
 end
 
 # Configure either initd or systemd
-if ::File.exist? "/etc/init.d/supervisord" then
-	# Managed processes sometimes don't shut down properly on daemon stop,
-	# leaving them 'orphaned' and resulting in duplicates.
-	# Work around by issuing a stop command to the children first.
-	execute "Stop children on supervisord stop" do
-		command <<-'SED'.strip + " /etc/init.d/supervisord"
-			sed -i 's/^\(\s*\)\(killproc\)/\1timeout 10s supervisorctl stop all || echo "WARNING: Unable to stop managed process(es) - check for orphans"; \2/'
-		SED
-	end
-else
+if system('which systemctl')
 	systemd_unit "supervisord.service" do
 		content({
 			Unit: {
@@ -71,5 +62,14 @@ else
 			}
 		})
 		action [:create, :enable]
+	end
+else
+	# Managed processes sometimes don't shut down properly on daemon stop,
+	# leaving them 'orphaned' and resulting in duplicates.
+	# Work around by issuing a stop command to the children first.
+	execute "Stop children on supervisord stop" do
+		command <<-'SED'.strip + " /etc/init.d/supervisord"
+			sed -i 's/^\(\s*\)\(killproc\)/\1timeout 10s supervisorctl stop all || echo "WARNING: Unable to stop managed process(es) - check for orphans"; \2/'
+		SED
 	end
 end
