304e153aacd430c41fb5489aeb0c2fde5c68fe0b
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-08-09 12:58:15 +1000

    [QOL-9055] point Nginx at uWSGI backend instead of Apache
    - This matches the recommended CKAN layout at https://docs.ckan.org/en/2.9/maintaining/installing/deployment.html
    including the uWSGI port number

--- recipes/ckanweb-deploy.rb
+++ recipes/ckanweb-deploy.rb
@@ -149,7 +149,7 @@ end
 # Create NGINX Config files
 #
 
-node.override['datashades']['app']['locations'] = "location ~ ^#{node['datashades']['ckan_web']['endpoint']} { proxy_pass http://localhost:8000; proxy_set_header Host $host; proxy_set_header X-Real-IP $remote_addr; proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; }"
+node.override['datashades']['app']['locations'] = "location ~ ^#{node['datashades']['ckan_web']['endpoint']} { proxy_pass http://localhost:8080; proxy_set_header Host $host; proxy_set_header X-Real-IP $remote_addr; proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; }"
 
 template "/etc/nginx/conf.d/#{node['datashades']['sitename']}-#{app['shortname']}.conf" do
 	source 'nginx.conf.erb'
