24d3b2c321fddcdb67fffd1fc91481e435cebfff
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-09-08 12:29:44 +1000

    [QOL-9055] pin 'pyOpenSSL' version so we support the maximum range of Python versions
    - ckanext-harvest 1.3.1 installs 18.0.0 by default, but that doesn't handle newer 'cryptography'

--- recipes/ckan-deploy.rb
+++ recipes/ckan-deploy.rb
@@ -75,6 +75,10 @@ execute "Pin setuptools version" do
 	command "#{virtualenv_dir}/bin/pip install setuptools==44.1.0"
 end
 
+execute "Pin pyOpenSSL version" do
+	command "#{virtualenv_dir}/bin/pip install pyOpenSSL==21.0.0"
+end
+
 datashades_pip_install_app "ckan" do
 	type app['app_source']['type']
 	revision app['app_source']['revision']
