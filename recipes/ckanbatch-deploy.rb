f3ff095cf5994c965c0e3880d4c7713268d14e99
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-03-08 10:06:53 +1000

    [QOLDEV-545] split CKAN-specific Parameter Store stack params to their own file
    - Solr servers do not have as many Parameter Store permissions as web or batch servers,
    so they shouldn't try to load CKAN-specific values.

--- recipes/ckanbatch-deploy.rb
+++ recipes/ckanbatch-deploy.rb
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "datashades::stackparams"
 include_recipe "datashades::ckan-deploy"
 
 service_name = "ckan"
