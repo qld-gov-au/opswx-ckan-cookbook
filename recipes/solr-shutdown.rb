0c760ccefd896c598f632fe987f3ab2975fd75b1
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-04-09 14:40:21 +1000

    [QOLDEV-545] ensure recipes include stackparams if they reference Chef node values

--- recipes/solr-shutdown.rb
+++ recipes/solr-shutdown.rb
@@ -20,6 +20,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe "datashades::stackparams"
+
 service_name = 'solr'
 
 file "/data/solr-healthcheck_#{node['datashades']['hostname']}" do
