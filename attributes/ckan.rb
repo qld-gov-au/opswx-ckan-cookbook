f02decc371d230e2a334b371ccac7c0a899d4dda
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-06-18 10:18:44 +1000

    [QOLDEV-892] allow PostgreSQL 15 client if no older version exists

--- attributes/ckan.rb
+++ attributes/ckan.rb
@@ -24,8 +24,8 @@
 default['datashades']['ckan_web']['endpoint'] = '/'
 default['datashades']['ckan_web']['packages'] = ['xml-commons', 'git', 'libxslt', 'libxslt-devel', 'libxml2', 'libxml2-devel', 'libxslt', 'libxslt-devel', 'gcc', 'gcc-c++', 'make', 'xalan-j2', 'unzip', 'squid']
 default['datashades']['ckan_web']['alternative_packages'] = [
-    ['postgresql94', 'postgresql'],
-    ['postgresql94-devel', 'postgresql-devel'],
+    ['postgresql', 'postgresql15'],
+    ['postgresql-devel', 'postgresql15-server-devel'],
     ['python27-devel', 'python3-devel'],
     ['policycoreutils-python', 'policycoreutils-python-utils'],
 ]
