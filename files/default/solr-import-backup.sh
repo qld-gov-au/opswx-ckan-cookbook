4db62fee07bcc7c6ad75709dc584a461fa6bcb4f
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-07-30 14:58:36 +1000

    [QOLSVC-10984] fix script syntax for detecting file presence

--- files/default/solr-import-backup.sh
+++ files/default/solr-import-backup.sh
@@ -11,7 +11,7 @@ SOLR_DIR=/var/solr/data/$CORE_NAME/data
 ARCHIVE_NAME=$1
 
 sudo systemctl stop solr
-sudo -u solr rm $SOLR_DIR/index.properties
+sudo -u solr rm -f $SOLR_DIR/index.properties
 sudo -u solr mkdir $SOLR_DIR/index
 sudo -u solr tar -C $SOLR_DIR/index -xvzf $ARCHIVE_NAME
 sudo systemctl start solr
