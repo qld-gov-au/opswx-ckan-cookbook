656da9e5234e830042a7d1c5a65cf4ab5f3456e8
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-08-05 09:18:29 +1000

    [QOLSVC-10262] clean up restore script
    - Drop heartbeat alteration since we're not constantly syncing anyway
    - Add documentation comments

--- files/default/solr-import-backup.sh
+++ files/default/solr-import-backup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Specify a (local) path to a Solr index snapshot archive.
+# This script will import the index into the local Solr instance.
+# This will cause a brief outage, as Solr will be stopped
+# and its index overwritten with the snapshot.
+# No attempt is made to interact with other Solr instances.
+
 if [ "$(whoami)" != "root" ] || [ "$#" -lt 1 ]; then
     echo "Usage: $0 <archive path>" >&2
     echo "$0 must be run as root"
@@ -11,6 +17,7 @@ fi
 SOLR_DIR=/var/solr/data/$CORE_NAME/data
 ARCHIVE_NAME=$1
 
+echo "Importing Solr index from ${ARCHIVE_NAME}..."
 systemctl stop solr
 sudo -u solr rm -f $SOLR_DIR/index.properties
 sudo -u solr mkdir $SOLR_DIR/index
